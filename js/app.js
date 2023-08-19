f251a6b61ae4b9ca16476523417756430a8850f9
rashidsarkar <rashidrock558@gmail.com>
2023-08-19 16:42:38 +0600

    update js

--- js/app.js
+++ js/app.js
@@ -6,15 +6,31 @@ function myWorkOnTextArea(className) {
 
 const fontSize = document.getElementById("fontSize");
 fontSize.value = 24;
-fontSize.addEventListener("click", function () {
+fontSize.addEventListener("click", function (e) {
+  e.stopPropagation();
   let mySizeFromUser = fontSize.value;
   let mySizeFromNum = parseInt(mySizeFromUser);
+  console.log(mySizeFromNum, mySizeFromUser);
 
-  textArea.classList.toggle(`text-[${mySizeFromNum}px]`);
+  textArea.style.fontSize = mySizeFromUser + "px";
 });
 const colorPicker = document.getElementById("colorPicker");
-colorPicker.addEventListener("input", function () {
+colorPicker.addEventListener("input", function (e) {
+  e.stopPropagation();
   let myColorString = colorPicker.value;
 
-  textArea.classList.toggle(`text-[${myColorString}]`);
+  textArea.style.color = myColorString;
 });
+
+const alingLeft = document.getElementById("aling-left");
+const alingright = document.getElementById("aling-right");
+const alingcenter = document.getElementById("aling-center");
+
+alingText("aling-left", "left");
+alingText("aling-right", "right");
+alingText("aling-center", "center");
+function alingText(elementId, elementAling) {
+  document.getElementById(elementId).addEventListener("click", function () {
+    textArea.style.textAlign = elementAling;
+  });
+}
